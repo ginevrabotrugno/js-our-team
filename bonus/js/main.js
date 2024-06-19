528c6e585b6c3f04a5894d3b74b94f43cd6edf14
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-19 09:42:14 +0200

    fix(bonus): fix cards order

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -5,24 +5,14 @@
 // Creare l’array di oggetti con le informazioni fornite.
 const membersList = [
     {
+        "name":"Wayne Barnett",
+        "role":"Founder CEO",
+        "pic":"wayne-barnett-founder-ceo.jpg",
+    },
+    {
         "name":"Angela Caroll",
         "role":"Chief Editor",
         "pic":"angela-caroll-chief-editor.jpg",
-    },
-    {
-        "name":"Angela Lopez",
-        "role":"Social Media Manager",
-        "pic":"angela-lopez-social-media-manager.jpg",
-    },
-    {
-        "name":"Barbara Ramos",
-        "role":"Graphic Designer",
-        "pic":"barbara-ramos-graphic-designer.jpg",
-    },
-    {
-        "name":"Scott Estrada",
-        "role":"Developer",
-        "pic":"scott-estrada-developer.jpg",
     },
     {
         "name":"Walter Gordon",
@@ -30,10 +20,20 @@ const membersList = [
         "pic":"walter-gordon-office-manager.jpg",
     },
     {
-        "name":"Wayne Barnett",
-        "role":"Founder CEO",
-        "pic":"wayne-barnett-founder-ceo.jpg",
-    }
+        "name":"Angela Lopez",
+        "role":"Social Media Manager",
+        "pic":"angela-lopez-social-media-manager.jpg",
+    },
+    {
+        "name":"Scott Estrada",
+        "role":"Developer",
+        "pic":"scott-estrada-developer.jpg",
+    },
+    {
+        "name":"Barbara Ramos",
+        "role":"Graphic Designer",
+        "pic":"barbara-ramos-graphic-designer.jpg",
+    }   
 ];
 
 // console.log(membersList);
